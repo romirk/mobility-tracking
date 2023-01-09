bd2032ecdcd19fcf6123e50a621ab0012fd7416c
Anthony Nikolaidis <anthony.nikolaidis@infopelago.gr>
2023-01-09 22:57:52 +0100

    Add graph

--- server/static/scripts/main.js
+++ server/static/scripts/main.js
@@ -10,6 +10,25 @@ socket.on('traffic_data', function(msg) {
     console.log(msg.data + ': ' + msg.count);
 })
 
-window.addEventListener('unload', function() {
-    socket.close();
+const ctx = document.getElementById('myChart');
+
+let chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00'],
+        datasets: [{
+            label: 'Number of cars over time',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            tension: 0.25,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
