404cfcfcdaf65986dbf8ba5b03b1d74cba5bc37b
romir kulshrestha <romir.kulshrestha@gmail.com>
2023-01-11 20:10:16 +0100

    sync requirements

--- server/backend.py
+++ server/backend.py
@@ -113,27 +113,6 @@ class Server:
         print("[INFO] stopping server")
 
 
-def rotate_image(mat, angle):
-    # angle in degrees
-
-    height, width = mat.shape[:2]
-    image_center = (width / 2, height / 2)
-
-    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
-
-    abs_cos = abs(rotation_mat[0, 0])
-    abs_sin = abs(rotation_mat[0, 1])
-
-    bound_w = int(height * abs_sin + width * abs_cos)
-    bound_h = int(height * abs_cos + width * abs_sin)
-
-    rotation_mat[0, 2] += bound_w / 2 - image_center[0]
-    rotation_mat[1, 2] += bound_h / 2 - image_center[1]
-
-    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
-    return rotated_mat
-
-
 if __name__ == "__main__":
     server = Server()
     server.run()
