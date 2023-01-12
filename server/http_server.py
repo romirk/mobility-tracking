eb56461a83466c9239ded6794b51812696229ca8
romir kulshrestha <romir.kulshrestha@gmail.com>
2023-01-12 22:58:52 +0100

    --visual

--- server/http_server.py
+++ server/http_server.py
@@ -6,12 +6,14 @@ from threading import Thread
 
 import cv2
 import numpy as np
+import yolov7_package
 from flask import Flask, render_template, Response, request
 from flask_socketio import SocketIO, emit
 
-from server.broker import detect
 from server.frame_server import FrameServer
-from server.utils import IMG_SIZE, decode64
+from server.utils import IMG_SIZE, decode64, encode64
+
+WHITELIST = {1: "bicycle", 2: "car", 3: "motorcycle", 5: "bus", 7: "truck"}
 
 
 class HttpServer:
@@ -29,6 +31,7 @@ class HttpServer:
         self.__last_frame: np.ndarray = np.ndarray(IMG_SIZE, dtype=np.uint8, buffer=self.__mem.buf)
 
         self.fs_thread = Thread(target=self.fs.run, daemon=True)
+        self.yolo = yolov7_package.Yolov7Detector()
         print("Server initialized")
 
     @property
@@ -42,6 +45,35 @@ class HttpServer:
         while self.__running.value:
             yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + \
                 cv2.imencode(".jpg", np.asarray(self.__last_frame).reshape(IMG_SIZE))[1].tobytes() + b"\r\n"
+
+    def detect(self, img):
+        cl, bx, sc = self.yolo.detect(img)
+        cl: list = cl[0]  # ugh
+
+        d = {"frame": encode64(img)}
+
+        for i in range(len(cl)):
+            if cl[i] not in WHITELIST or sc[i] < 0.7:
+                continue
+            if cl[i] not in d:
+                d[cl[i]] = {
+                    "name": WHITELIST[cl[i]],
+                    "boxes": [bx[i]],
+                    "scores": [sc[i]],
+                    "count": 1
+                }
+            else:
+                d[cl[i]]["boxes"].append(bx[i])
+                d[cl[i]]["scores"].append(sc[i])
+                d[cl[i]]["count"] += 1
+
+        s = ""
+        for c in WHITELIST:
+            if c in d:
+                name, count = d[c]['name'], d[c]['count']
+                s += f"{count} {name}{'' if count == 1 else 's'} "
+        d["str"] = s if len(s) else "no detections."
+        return d
 
     def create_server(self):
         app = Flask(__name__)
@@ -67,7 +99,7 @@ class HttpServer:
             frame = decode64(data["frame"])
             x, y, w, h = decode64(data["rect"])
             cropped = frame[y:y + h, x:x + w]
-            result = detect(cropped)
+            result = self.detect(cropped)
             print(result["str"])
             socketio.emit('detect', {
                 "count": data["count"],
--- server/utils.py
+++ server/utils.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import codecs
 import pickle
 
-IMG_SIZE = (480, 640, 3)
+IMG_SIZE = (480, 640)
+IMG_SIZE_RGB = (480, 640, 3)
 
 
 def encode64(data: object):
