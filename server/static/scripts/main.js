c51bab8f42d358cf65ac04051ce33ec27e96ea21
Anthony Nikolaidis <anthony.nikolaidis@infopelago.gr>
2023-01-15 16:26:44 +0100

    Make UI update on new data

--- server/static/scripts/main.js
+++ server/static/scripts/main.js
@@ -14,12 +14,14 @@ class SensorLive {
         this.socket.on('sensor', this.on_sensor.bind(this));
         this.socket.on('detect', this.on_detect.bind(this));
 
-        this.ctxNumberOfVehicles = document.getElementById('numberOfVehicles');
-        this.numberOfVehicles = new Chart(this.ctxNumberOfVehicles, {
-            type: 'line', data: {
-                labels: ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00'], datasets: [{
-                    label: 'Number of cars over time',
-                    data: [65, 59, 80, 81, 56, 55, 40],
+        this.ctxvehiclesOverTime = document.getElementById('numberOfVehicles');
+        this.vehiclesOverTime = new Chart(this.ctxvehiclesOverTime, {
+            type: 'line',
+            data: {
+                labels: ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                datasets: [{
+                    label: 'Number of vehicles over time (updates every 10 seconds)',
+                    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     fill: false,
                     tension: 0.35,
                     borderWidth: 3
@@ -33,12 +35,14 @@ class SensorLive {
             }
         });
 
-        this.ctxTypesOfVehicles = document.getElementById('typesOfVehicles');
-        this.numberOfVehicles = new Chart(this.ctxTypesOfVehicles, {
-            type: 'doughnut', data: {
-                labels: ['Cars', 'Trucks', 'Buses'], datasets: [{
+        this.ctxNumberOfVehicles = document.getElementById('typesOfVehicles');
+        this.numberOfVehicles = new Chart(this.ctxNumberOfVehicles, {
+            type: 'doughnut',
+            data: {
+                labels: ['Cars', 'Trucks', 'Buses'],
+                datasets: [{
                     label: 'Types of vehicles',
-                    data: [300, 50, 100],
+                    data: [0.0000001, 0.0000001, 0.0000001],
                     backgroundColor: ['rgb(2, 159, 227)', 'rgb(26,122,165)', 'rgb(31,84,108)'],
                     hoverOffset: 4
                 }]
@@ -48,6 +52,7 @@ class SensorLive {
         });
 
         setInterval(this.update.bind(this), 1000);
+        setInterval(this.updateGraphs.bind(this), 10000);
     }
 
     on_connect() {
@@ -76,6 +81,10 @@ class SensorLive {
         document.getElementById('counts_car').innerHTML = this.counts.car.total;
         document.getElementById('counts_bus').innerHTML = this.counts.bus.total;
         document.getElementById('counts_truck').innerHTML = this.counts.truck.total;
+        document.getElementById('total_count_2').innerHTML = this.counter;
+        document.getElementById('counts_car_2').innerHTML = this.counts.car.total;
+        document.getElementById('counts_bus_2').innerHTML = this.counts.bus.total;
+        document.getElementById('counts_truck_2').innerHTML = this.counts.truck.total;
         // document.getElementById('sensor_pm10').innerHTML = this.sensor_data.pm10;
         // document.getElementById('sensor_pm25').innerHTML = this.sensor_data.pm25;
         // document.getElementById('sensor_pm50').innerHTML = this.sensor_data.pm50;
@@ -84,8 +93,30 @@ class SensorLive {
         // document.getElementById('sensor_humidity').innerHTML = this.sensor_data.humidity;
         // document.getElementById('sensor_co2').innerHTML = this.sensor_data.co2;
 
-        // Update chart
-        // TODO
+        this.numberOfVehicles.data.datasets.forEach((dataset) => {
+            dataset.data = [this.counts.car.total, this.counter.truck.total, this.counter.bus.total];
+        });
+
+        this.numberOfVehicles.update();
+    }
+
+    updateGraphs() {
+        // Update chart [VEHICLES OVER TIME]
+        let date = new Date()
+
+        this.vehiclesOverTime.data.labels.shift();
+        this.vehiclesOverTime.data.labels.push(
+            date.getHours().toString() + ":" +
+            date.getMinutes().toString() + ":" +
+            date.getSeconds().toString()
+        );
+
+        this.vehiclesOverTime.data.datasets.forEach((dataset) => {
+            dataset.data.shift();
+            dataset.data.push(this.counter);
+        });
+
+        this.vehiclesOverTime.update();
     }
 
     log() {
