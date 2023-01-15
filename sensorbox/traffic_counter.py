8f803090cf0b5a1673cfc4aacd9662450bcf0310
Anthony Nikolaidis <anthony.nikolaidis@infopelago.gr>
2023-01-15 01:53:11 +0100

    increase blurring

--- sensorbox/traffic_counter.py
+++ sensorbox/traffic_counter.py
@@ -285,7 +285,7 @@ class TrafficCounter(object):
 
                 # Background subtraction
                 fg_mask = self.bg_subtractor.apply(working_img, background_avg, rate_of_influence)
-                blurred = cv2.GaussianBlur(fg_mask, (10, 10), 0)
+                blurred = cv2.GaussianBlur(fg_mask, (11, 11), 0)
                 ret1, th1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY)
                 dilated = cv2.dilate(th1, None)
                 final_img = cv2.dilate(dilated, None)
