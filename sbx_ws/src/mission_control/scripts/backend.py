59e654cc208a25e8963e5b07c9f98e0d2dfcfa2b
romir kulshrestha <romir.kulshrestha@gmail.com>
2023-02-07 09:10:21 +0100

    ros enabled server

---
+++ sbx_ws/src/mission_control/scripts/backend.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from sbx_server import SbxServer
+
+if __name__ == "__main__":
+    server = SbxServer()
+    server.run()
--- server/backend.py
+++
@@ -1,6 +0,0 @@
-#!./env/bin/python
-from http_server import HttpServer
-
-if __name__ == "__main__":
-    server = HttpServer()
-    server.run()
