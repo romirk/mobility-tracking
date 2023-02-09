58767b8829db8552aa31b5e0c0f2a103dff925d7
romir kulshrestha <romir.kulshrestha@gmail.com>
2023-02-09 12:36:35 +0100

    box pub

--- sbx_ws/src/sensorbox/scripts/traffic_counter.py
+++ sbx_ws/src/sensorbox/scripts/traffic_counter.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 """
-Traffic Counting
+Mobility Tracking
+
+Copyright (C) 2023 YES!Delft
+by Romir Kulshreshtha and Anthony Nikolaidis
 """
+
 import time
 from multiprocessing import Event
 
@@ -11,18 +15,16 @@ import numpy as np
 import rospy
 from sensor_msgs.msg import Image
 from vision_msgs.msg import Detection2D
-from ros_numpy import msgify, numpify
-
-# from jetson_inference import detectNet
-
-
-# TEST_FILE = "./mobility-tracking/ignore/test.mp4"
-TEST_FILE = "./test.mp4"
+from sensorbox.msg import Box
+from ros_numpy.src.ros_numpy.image import numpy_to_image, image_to_numpy
 
 
-class TrafficCounter:
+class MobilityTracker:
     """
-    Traffic Counter class.
+    Mobility Tracker
+
+    This class is responsible for detecting and tracking objects in a video stream.
+    For the 2023 YES!Delft Region of the Future project.
     """
 
     def __init__(self):
@@ -65,7 +67,10 @@ class TrafficCounter:
         self.frame_sub = rospy.Subscriber(
             f"/{self.camera_name}/color/image_raw", Image, self.frame_callback
         )
-        self.detection_pub = rospy.Publisher(f"/{self.prefix}/detect", Detection2D, queue_size=10)
+        self.detection_pub = rospy.Publisher(
+            f"/{self.prefix}/detect", Detection2D, queue_size=10
+        )
+        self.boxes_pub = rospy.Publisher(f"/{self.prefix}/bounds", Box, queue_size=10)
 
     def _set_up_line(self, line_direction, line_position):
         if line_direction.upper() == "H" or line_direction is None:
@@ -80,50 +85,31 @@ class TrafficCounter:
         else:
             raise ValueError('Expected an "H" or a "V" only for line direction')
 
-    def _draw_bounding_boxes(
-        self, frame, contour_id, bounding_points, cx, cy, prev_cx, prev_cy, area
-    ):
-        cv2.drawContours(frame, [bounding_points], 0, (0, 255, 0), 1)
-        cv2.line(
-            frame, (prev_cx, prev_cy), (cx, cy), (0, 0, 255), 1
-        )  # line between last position and current position
-        cv2.circle(frame, (cx, cy), 3, (0, 0, 255), 4)
-        cv2.putText(
-            frame,
-            str(contour_id) + " " + str(area),
-            (cx, cy - 15),
-            self.font,
-            0.4,
-            (255, 0, 0),
-            2,
-        )
-
-    def _is_line_crossed(self, frame, cx, cy, prev_cx, prev_cy):
+    def _is_line_crossed(self, cx, cy, prev_cx, prev_cy):
         if self.line_direction.upper() == "H":
             if (prev_cy <= self.p1_count_line[1] <= cy) or (
                 cy <= self.p1_count_line[1] <= prev_cy
             ):
                 self.counter += 1
-                cv2.line(frame, self.p1_count_line, self.p2_count_line, (0, 255, 0), 5)
                 return True, "u" if cy - self.p1_count_line[1] < 0 else "d"
         elif self.line_direction.upper() == "V":
             if (prev_cx <= self.p1_count_line[0] <= cx) or (
                 cx <= self.p1_count_line[0] <= prev_cx
             ):
                 self.counter += 1
-                cv2.line(frame, self.p1_count_line, self.p2_count_line, (0, 255, 0), 5)
                 return True, "l" if cx - self.p1_count_line[0] < 0 else "r"
         return False, None
 
     # def classify(self, frame, bounding_box):
     #     detections = self.net.Detect(frame, overlay="box,labels,conf")
 
-    def bind_objects(self, frame, thresh_img):
-        """Draws bounding boxes and detects when cars are crossing the line frame: numpy image where boxes will be
+    def bind_objects(self, incoming_frame: Image, thresh_img):
+        """
+        Draws bounding boxes and detects when cars are crossing the line frame: numpy image where boxes will be
         drawn onto thresh_img: numpy image after subtracting the background and all thresholds and noise reduction
         operations are applied
         """
-        orig_frame = frame.copy()
+        orig_frame = image_to_numpy(incoming_frame.data)
         contours, _ = cv2.findContours(
             thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
         )  # this line is for opencv 2.4, and also now for OpenCV 4.4, so this is the current one
@@ -141,7 +127,7 @@ class TrafficCounter:
             rect = cv2.minAreaRect(c)
             bounding = cv2.boundingRect(c)
             points = cv2.boxPoints(rect)  # This is the way to do it in opencv 3.1
-            points = np.int0(points)
+            points = np.intp(points)
 
             # Getting the center coordinates of the contour box
             cx = int(rect[0][0])
@@ -161,7 +147,7 @@ class TrafficCounter:
             elif len(contours) == 0:
                 prev_cx, prev_cy = cx, cy
             else:
-                min_point = None
+                min_point = None, None
                 min_dist = None
                 for i in range(len(self.prev_centroids)):
                     dist = np.linalg.norm(
@@ -177,9 +163,7 @@ class TrafficCounter:
                     prev_cx, prev_cy = cx, cy
                 # prev_cx,prev_cy = min_point
 
-            _is_crossed, direction = self._is_line_crossed(
-                frame, cx, cy, prev_cx, prev_cy
-            )
+            _is_crossed, direction = self._is_line_crossed(cx, cy, prev_cx, prev_cy)
             if _is_crossed:
                 detection_msg = Detection2D()
                 detection_msg.header.frame_id = "camera"
@@ -188,12 +172,16 @@ class TrafficCounter:
                 detection_msg.bbox.center.y = cy
                 detection_msg.bbox.size_x = w
                 detection_msg.bbox.size_y = h
-                detection_msg.source_img = msgify(Image, orig_frame, encoding="bgr8")
+                detection_msg.source_img = numpy_to_image(orig_frame, "bgr8")
                 self.detection_pub.publish(detection_msg)
-
-            self._draw_bounding_boxes(
-                frame, cnt_id, points, cx, cy, prev_cx, prev_cy, area
-            )
+            box_msg = Box()
+            box_msg.header = incoming_frame.header
+            box_msg.box.center.x = bounding[0] + bounding[2] / 2
+            box_msg.box.center.y = bounding[1] + bounding[3] / 2
+            box_msg.box.center.theta = 0
+            box_msg.box.size_x = bounding[2]
+            box_msg.box.size_y = bounding[3]
+            self.boxes_pub.publish(box_msg)
 
             cnt_id += 1
         self.prev_centroids = cur_centroids  # updating centroids for next frame
@@ -228,15 +216,9 @@ class TrafficCounter:
 
         # Drawing bounding boxes and counting
         t2 = time.time()
-        self.bind_objects(img, final_img)
-
-        # colored_final_img = img
-
-        # down_sampled = cv2.resize(colored_final_img, (640, 480))
-
-        # todo publish image
+        self.bind_objects(frame, final_img)
 
 
 if __name__ == "__main__":
-    t = TrafficCounter()
+    t = MobilityTracker()
     rospy.spin()
