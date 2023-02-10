4a2e7329ca2cae516bcec2ddc06e87595d3e30bf
romir kulshrestha <romir.kulshrestha@gmail.com>
2023-02-11 00:31:08 +0100

    basic db

--- sbx_ws/src/mission_control/scripts/yolo_server.py
+++ sbx_ws/src/mission_control/scripts/yolo_server.py
@@ -12,12 +12,18 @@ from mission_control.msg import Counts
 from vision_msgs.msg import Detection2D
 from yolov7_package import Yolov7Detector
 from yolov7_package.model_utils import coco_names
+import sqlite3 as sl
 import os
+from threading import Lock
 
 WHITELIST = ["car", "truck", "bus", "motorbike", "bicycle"]
 WHITELIST_IDX = [coco_names.index(c) for c in WHITELIST]
 CONFIDENCE_THRESHOLD = 0  # TODO: set this to 0.5
 ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+STORE_COUNT_STMT = "INSERT INTO counts (timestamp, total, cars, trucks, buses, motorcycles, bicycles) VALUES (?, ?, ?, ?, ?, ?, ?)"
+LOAD_COUNT_STMT = "SELECT * FROM counts ORDER BY timestamp DESC LIMIT 1"
 
 
 class YoloServer:
@@ -34,12 +40,32 @@ class YoloServer:
         self.yolo = Yolov7Detector(traced=True)
         self.counts = Counts(0, 0, 0, 0, 0, 0)
 
+        # TODO move this to a more robust database
+        self.con = sl.connect("mobility-tracking.db", check_same_thread=False)
+        self.cur = self.con.cursor()
+        self.lock = Lock()
+
+        # load last count
+        try:
+            self.cur.execute(LOAD_COUNT_STMT)
+        except sl.OperationalError:
+            self.cur.execute(
+                "CREATE TABLE counts (timestamp DATETIME, total INT, cars INT, trucks INT, buses INT, motorcycles INT, bicycles INT)"
+            )
+            self.cur.execute(STORE_COUNT_STMT, (datetime.now(), 0, 0, 0, 0, 0, 0))
+            self.con.commit()
+            self.cur.execute(LOAD_COUNT_STMT)
+        row = self.cur.fetchone()
+        if row:
+            self.counts = Counts(*row[1:])
+        # self.con.close()
 
         if self.multiprocessing:
             self.executor = ProcessPoolExecutor(max_workers=5)
             self.tasks = {}
 
         self.pub = rospy.Publisher("/sbx/result", Counts, queue_size=10, latch=True)
+        self.pub.publish(self.counts)
         if self.multiprocessing:
             self.sub = rospy.Subscriber(
                 "/sbx/detect", Detection2D, self.detect_callback
@@ -48,13 +74,20 @@ class YoloServer:
             self.sub = rospy.Subscriber("/sbx/detect", Detection2D, self.exec_callback)
 
     def __del__(self) -> None:
+        self.con.close()
         if self.multiprocessing:
             self.executor.shutdown()
 
     def exec_callback(self, msg: Detection2D) -> tuple[int, int, int, int, int, int]:
-        total = self.counts.total + 1
         box = msg.bbox
+        direction = "backward" if box.center.theta else "forward"
         frame = msg.source_img
+
+        rospy.loginfo(
+            f"Detected object moving {direction} at {box.center.x}, {box.center.y}"
+        )
+
+        total = self.counts.total + 1
         img = cv2.cvtColor(
             np.frombuffer(frame.data, dtype=np.uint8).reshape(
                 frame.height, frame.width, -1
@@ -71,17 +104,22 @@ class YoloServer:
         cx, cy = x + w / 2, y + h / 2
 
         # cv2.rectangle(img, (int(x), int(y)), (int(x + w), int(y + h)), (0, 255, 0), 2)
-        cv2.imwrite(f"{ROOT}/out/{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}_{total:04}.png", img)
+        cv2.imwrite(
+            f"{ROOT}/out/{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}_{total:04}.png",
+            img,
+        )
         r = self.yolo.detect(img)
         dets = [
             d
             for d in zip(r[0][0], r[1][0], r[2][0])
-            if d[0] in WHITELIST_IDX and d[2] > CONFIDENCE_THRESHOLD
+            if d[0] in WHITELIST_IDX  # and d[2] > CONFIDENCE_THRESHOLD
         ]
         rospy.loginfo(f"Filtered detections: {dets}")
 
         if len(dets) == 0:
-            rospy.logwarn("Sensorbox reports a detection, but YOLO did not detect a vehicle")
+            rospy.logwarn(
+                "Sensorbox reports a detection, but YOLO did not detect a vehicle"
+            )
             result = (
                 total,
                 self.counts.cars,
@@ -90,29 +128,30 @@ class YoloServer:
                 self.counts.motorcycles,
                 self.counts.bicycles,
             )
-            self.counts = Counts(*result)
-            self.pub.publish(self.counts)
-            return result
-
-        boxes = [(d[1][0] - cx) ** 2 + (d[1][1] - cy) ** 2 for d in dets]
-        nearest = np.argmin(boxes)
-        rospy.loginfo(f"Nearest box: {nearest}")
-        nearest_class: int = dets[nearest][0]
-        result = (
-            total,
-            self.counts.cars + (nearest_class == 2),
-            self.counts.trucks + (nearest_class == 7),
-            self.counts.buses + (nearest_class == 5),
-            self.counts.motorcycles + (nearest_class == 4),
-            self.counts.bicycles + (nearest_class == 1),
-        )
-        rospy.loginfo(
-            f"Detected {coco_names[nearest_class]} at {box.center.x}, {box.center.y}"
-        )
+        else:
+            boxes = [(d[1][0] - cx) ** 2 + (d[1][1] - cy) ** 2 for d in dets]
+            nearest = np.argmin(boxes)
+            rospy.loginfo(f"Nearest box: {nearest}")
+            nearest_class: int = dets[nearest][0]
+            result = (
+                total,
+                self.counts.cars + (nearest_class == 2),
+                self.counts.trucks + (nearest_class == 7),
+                self.counts.buses + (nearest_class == 5),
+                self.counts.motorcycles + (nearest_class == 4),
+                self.counts.bicycles + (nearest_class == 1),
+            )
+            rospy.loginfo(
+                f"Detected {coco_names[nearest_class]} at {box.center.x}, {box.center.y}"
+            )
 
         if not self.multiprocessing:
             self.counts = Counts(*result)
             self.pub.publish(self.counts)
+
+            with self.lock:
+                self.cur.execute(STORE_COUNT_STMT, (datetime.now(), *result))
+                self.con.commit()
 
         return result
 
